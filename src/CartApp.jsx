a8c1a43fad497166416c4acfc9b8f74840a7befe
Memobig <memobig@gmail.com>
2024-05-02 19:16:54 -0600

    hook personalizado useItemsCart

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,51 +1,11 @@
-import { useEffect, useReducer, useState } from "react"
 import { CartView } from "./components/CartView"
 import { CatalogView } from "./components/CatalogView"
-import { itemsReducer } from "./reducer/itemsReducer";
-import { AddProductCart, DeleteProductCart, UpdateQuantityProductCart } from "./reducer/itemsActions";
+import { useItemsCart } from "./hooks/useItemsCart"
 
-const initialCartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
 
 export const CartApp = () => {
 
-    const [cartItems, dispatch] = useReducer(itemsReducer, initialCartItems);
-
-    useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cartItems));
-    }, [cartItems])
-    const handlerAddProductCart = (product) => {
-
-        const hasItem = cartItems.find((i) => i.product.id === product.id);
-        if (hasItem) {
-
-            dispatch(
-                {
-                    type: UpdateQuantityProductCart,
-                    payload: product,
-                }
-            )
-        } else {
-
-            dispatch(
-                {
-                    type: AddProductCart,
-                    payload: product,
-                }
-            )
-        }
-
-    }
-
-    const handlerDeleteProductCart = (id) => {
-        dispatch(
-            {
-                type: DeleteProductCart,
-                payload: id,
-            }
-        )
-    }
-
-
+    const { cartItems, handlerAddProductCart, handlerDeleteProductCart } = useItemsCart();
     return (
         <>
             <div className="container my-4">
