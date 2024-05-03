047c0675f99ba81170511d3e6a8cf41f0ffe9908
Memobig <memobig@gmail.com>
2024-05-02 22:23:51 -0600

    se mapean las rutas catalog y cart con Routes

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,3 +1,4 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import { CartView } from "./components/CartView"
 import { CatalogView } from "./components/CatalogView"
 import { useItemsCart } from "./hooks/useItemsCart"
@@ -10,13 +11,27 @@ export const CartApp = () => {
         <>
             <div className="container my-4">
                 <h3>CartApp</h3>
-                <CatalogView handler={handlerAddProductCart} />
-                {cartItems?.length <= 0 || (
+                <Routes>
+                    <Route
+                        path="catalog"
+                        element={<CatalogView handler={handlerAddProductCart} />} />
+                    <Route
+                        path="cart"
+                        element={(
+                            cartItems?.length <= 0 ?
+                            <div className="alert alert-warning">No hay productos en el carrito de compras!</div>
+                            :
+                            (
 
-                    <div className="my-4 w-50">
-                        <CartView handlerDelete={handlerDeleteProductCart} items={cartItems} />
-                    </div>
-                )}
+                                <div className="my-4 w-50">
+                                    <CartView handlerDelete={handlerDeleteProductCart} items={cartItems} />
+                                </div>
+                            )
+                        )} />
+                        <Route path="/" element={ <Navigate to='/catalog' />} />
+                </Routes>
+
+
             </div>
         </>
     )
